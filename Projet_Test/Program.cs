1ca3ec041c266b26343e24c76bf5529743a653bd
OlivierBH <1732451@etu.cchic.ca>
2023-08-28 14:32:46 -0400

    modif 1

--- Projet_Test/Program.cs
+++ Projet_Test/Program.cs
@@ -4,9 +4,20 @@ namespace Projet_Test
 {
     internal class Program
     {
+       
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+          
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+
+            Random random = new Random();
+            int stupidityPercentage = random.Next(0, 101);
+
+            Console.WriteLine($"Damn {userName}, your name sounds {stupidityPercentage}% stupid.");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
